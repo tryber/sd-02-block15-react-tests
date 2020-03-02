f3199e2404785914ffcce72acb40cbcb90a6bfe4
richardwellerson <richardwellerson@gmail.com>
2020-03-02 14:24:58 -0300

    6 teste feito

---
+++ src/tests/6.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { cleanup } from '@testing-library/react';
+import allPokemons from '../services/pokedexDataTest';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
+
+afterEach(cleanup);
+
+describe('6.', () => {
+  test('Pokedex create one filter for type of pokemon', () => {
+    const { getAllByText, getByText } = renderWithRouter(<App />);
+    const pokemonTypes = new Set(allPokemons.map(({ type }) => type));
+    const allButton = getByText(/All/i);
+    pokemonTypes.forEach((type) => {
+      const actualType = getAllByText(type)[1] || getByText(type);
+      expect(actualType).toBeInTheDocument();
+      expect(allButton).toBeInTheDocument();
+    });
+  });
+});
