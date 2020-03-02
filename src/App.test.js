49d103c8e6615df1acaaec9a5cb369e0c0ae974c
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-03-02 18:09:57 -0300

    Requisito 2 e 3 feitos

--- src/App.test.js
+++ src/App.test.js
@@ -1,24 +1,55 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { cleanup, fireEvent, render } from '@testing-library/react';
 import App from './App';
+import pokemonsMock from './services/mockPkmnData';
 
-test('shows the Pokedéx when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
-      <App />
-    </MemoryRouter>,
-  );
+afterEach(cleanup);
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
-});
+describe('Pokemon app.js tests', () => {
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+  test('0 - Renders a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('1 - Ao carregar a aplicação no caminho de URL “/”, a página principal da Pokédex deve ser mostrada.', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>,
+    );
+
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  });
+
+  test('2 - A Pokédex deve exibir apenas um pokémon por vez', () => {
+    const { getAllByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const weight = getAllByText(/Average weight:/i);
+    expect(weight.length).toBe(1);
+  });
+
+  test('3 - Ao apertar o botão de próximo, a página deve exibir o próximo pokémon da lista', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const nextBtn = getByText(/Próximo pokémon/i);
+    expect(nextBtn).toBeInTheDocument();
+    pokemonsMock.forEach((pokemon) => {
+      expect(getByText(pokemon.name)).toBeInTheDocument();
+      fireEvent.click(nextBtn);
+    });
+    expect(getByText(pokemonsMock[0].name)).toBeInTheDocument();
+  });
 });
