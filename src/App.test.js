93fee9d2f5aada5e2c26a2cd1b32cda13ca9666e
Douglas Henrique <doughrqe@gmail.com>
2020-03-02 02:16:08 -0300

    Add validation with altImage

--- src/App.test.js
+++ src/App.test.js
@@ -36,7 +36,7 @@ describe('Pokemon test', () => {
   });
 
   test('When pressing the next button, the page should display the next pokémon in the list', () => {
-    const { getByText, queryByText } = render(
+    const { getByText, queryByText, getByAltText } = render(
       <MemoryRouter>
         <App />
       </MemoryRouter>,
@@ -46,37 +46,46 @@ describe('Pokemon test', () => {
     fireEvent.click(getByText(/Próximo pokémon/));
     expect(getByText(/Charmander/)).toBeInTheDocument();
     expect(queryByText('Average weight: 8.5 kg')).toBeInTheDocument();
+    expect(getByAltText(/Charmander sprite/)).toBeInTheDocument();
     // Caterpie
     fireEvent.click(getByText(/Próximo pokémon/));
     expect(getByText(/Caterpie/)).toBeInTheDocument();
     expect(queryByText('Average weight: 2.9 kg')).toBeInTheDocument();
+    expect(getByAltText(/Caterpie sprite/)).toBeInTheDocument();
     // Ekans
     fireEvent.click(getByText(/Próximo pokémon/));
     expect(getByText(/Ekans/)).toBeInTheDocument();
     expect(queryByText('Average weight: 6.9 kg')).toBeInTheDocument();
+    expect(getByAltText(/Ekans sprite/)).toBeInTheDocument();
     // Alakazam
     fireEvent.click(getByText(/Próximo pokémon/));
     expect(getByText(/Alakazam/)).toBeInTheDocument();
     expect(queryByText('Average weight: 48.0 kg')).toBeInTheDocument();
+    expect(getByAltText(/Alakazam sprite/)).toBeInTheDocument();
     // Mew
     fireEvent.click(getByText(/Próximo pokémon/));
     expect(getByText(/Mew/)).toBeInTheDocument();
     expect(queryByText('Average weight: 4.0 kg')).toBeInTheDocument();
+    expect(getByAltText(/Mew sprite/)).toBeInTheDocument();
     // Rapidash
     fireEvent.click(getByText(/Próximo pokémon/));
     expect(getByText(/Rapidash/)).toBeInTheDocument();
     expect(queryByText('Average weight: 95.0 kg')).toBeInTheDocument();
+    expect(getByAltText(/Rapidash sprite/)).toBeInTheDocument();
     // Snorlax
     fireEvent.click(getByText(/Próximo pokémon/));
     expect(getByText(/Snorlax/)).toBeInTheDocument();
     expect(queryByText('Average weight: 460.0 kg')).toBeInTheDocument();
+    expect(getByAltText(/Snorlax sprite/)).toBeInTheDocument();
     // Dragonair
     fireEvent.click(getByText(/Próximo pokémon/));
     expect(getByText(/Dragonair/)).toBeInTheDocument();
     expect(queryByText('Average weight: 16.5 kg')).toBeInTheDocument();
+    expect(getByAltText(/Dragonair sprite/)).toBeInTheDocument();
     // Pikachu
     fireEvent.click(getByText(/Próximo pokémon/));
     expect(getByText(/Pikachu/)).toBeInTheDocument();
     expect(queryByText('Average weight: 6.0 kg')).toBeInTheDocument();
+    expect(getByAltText(/Pikachu sprite/)).toBeInTheDocument();
   });
 });
