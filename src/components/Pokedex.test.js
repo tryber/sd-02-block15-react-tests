1289d09e808a7b91c76be13d73809a898e9d490f
phtf92 <phtf92@gmail.com>
2020-03-02 20:35:33 -0300

    Requisito 5 e 6

--- src/components/Pokedex.test.js
+++ src/components/Pokedex.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render, cleanup, fireEvent } from '@testing-library/react';
 import App from '../App';
+import Pokedex from './Pokedex';
 import pokemons from '../data';
 
 afterEach(cleanup);
@@ -15,6 +16,22 @@ const arrayOfPokemonsTypes = pokemons.reduce((pv, cv) => {
   pv.push(cv.type);
   return pv;
 }, []);
+
+const pokemonsFiltered = () => pokemons.filter(({ type }) => {
+  return (type === 'Electric' || type === 'Fire' || type === 'Normal' || type === 'Psychic');
+});
+
+const Pokemonfavorited = {
+  25: true,
+  4: true,
+  10: true,
+  23: false,
+  65: true,
+  151: true,
+  78: false,
+  143: true,
+  148: false,
+};
 
 
 test('2 - Pokedex shows only one pokemon each time', () => {
@@ -97,4 +114,55 @@ describe('4 - Pokedex should contain filter buttons', () => {
       expect(btnNameType).toBeInTheDocument();
     });
   });
+});
+
+describe('5 - Pokedex should contain reset button filter', () => {
+  test('5.1 - The text should be "All"', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>,
+    );
+    expect(getByText('All')).toBeInTheDocument();
+  });
+  const func5 = (estadoinicial) => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>,
+    );
+    const btnAllClick = getByText('All');
+    const nextPokemonButton = getByText('Próximo pokémon');
+    if (!estadoinicial) {
+      fireEvent.click(btnAllClick);
+    }
+    arrayOfPokemonsName.forEach(() => {
+      fireEvent.click(nextPokemonButton);
+    });
+    expect(getByText(arrayOfPokemonsName[0])).toBeInTheDocument();
+  };
+
+  test('5.2 - After clicking it, it should return to the initial state', () => {
+    func5(false);
+  });
+
+  test('5.3 - By default, it starts with all filter selected', () => {
+    func5(true);
+  });
+});
+
+test('6 - Generate dinamic button to type', () => {
+  const { getByText, getAllByText, queryByText } = render(
+    <MemoryRouter initialEntries={['/']}>
+      <Pokedex
+        pokemons={pokemonsFiltered()}
+        isPokemonFavoriteById={Pokemonfavorited}
+      />
+    </MemoryRouter>,
+  );
+  expect(getAllByText('Electric')[1]).toBeInTheDocument();
+  expect(getByText('Normal')).toBeInTheDocument();
+  expect(getByText('Psychic')).toBeInTheDocument();
+  expect(getByText('Fire')).toBeInTheDocument();
+  expect(queryByText('Bug')).not.toBeInTheDocument();
 });
