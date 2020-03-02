da2dab34f394a4283612a5ebda0bdfaa5cf3a9b5
Douglas Henrique <doughrqe@gmail.com>
2020-03-02 18:46:56 -0300

    requisito 12

--- src/App3.test.js
+++ src/App3.test.js
@@ -8,7 +8,7 @@ import pokemons from './data';
 afterEach(cleanup);
 
 describe('Pokemon test 11 - 15', () => {
-  test('The Pokémon details page should display the name, type, average weight and image of the Pokémon displayed', () => {
+  test('The Pokémon details page should display the name, type, average weight and image of the Pokémon displayed and not Product Details link', () => {
     const history = createMemoryHistory();
     const { queryByText, queryByAltText } = render(
       <Router history={history}>
@@ -26,6 +26,7 @@ describe('Pokemon test 11 - 15', () => {
       expect(queryByText(`Average weight: ${value} ${measurementUnit}`)).toBeInTheDocument();
       expect(queryByAltText(`${name} sprite`)).toBeInTheDocument();
       expect(queryByAltText(`${name} sprite`).src).toBe(image);
+      expect(queryByText('More details')).toBeNull();
       history.push('/');
       for (let i = 0; i < index + 1; i += 1) {
         fireEvent.click(queryByText('Próximo pokémon'));
