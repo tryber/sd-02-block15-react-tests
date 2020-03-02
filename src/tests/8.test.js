c92e0004daa5ada47abf764fe217f16f69cbdee9
richardwellerson <richardwellerson@gmail.com>
2020-03-02 15:24:24 -0300

    Teste 8 feito

---
+++ src/tests/8.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { cleanup, fireEvent } from '@testing-library/react';
+import allPokemons from '../services/pokedexDataTest';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
+
+afterEach(cleanup);
+
+describe('8.', () => {
+  test('Pokedex shows name, type and average weight of each pokemon', () => {
+    const { getByText, queryAllByText, queryByText } = renderWithRouter(<App />);
+    const nextButton = getByText(/Próximo pokémon/i);
+    allPokemons.forEach((pokemon) => {
+      const pokemonName = getByText(pokemon.name);
+      const pokemonType = queryAllByText(pokemon.type)[0];
+      const pokemonWeight = queryByText(`Average weight: ${pokemon.averageWeight.value} ${pokemon.averageWeight.measurementUnit}`);
+      expect(pokemonName).toBeInTheDocument();
+      expect(pokemonType).toBeInTheDocument();
+      expect(pokemonWeight).toBeInTheDocument();
+      fireEvent.click(nextButton);
+    });
+  });
+});
