d1ad3b7f916e282c6c7819ce5ca9776b2ce5eaec
richardwellerson <richardwellerson@gmail.com>
2020-03-02 14:13:30 -0300

    Teste 5 feito

--- src/tests/5.test.js
+++ src/tests/5.test.js
@@ -6,14 +6,19 @@ import App from '../App';
 
 afterEach(cleanup);
 
-describe('3.', () => {
-  test('Pressing next button, pokedex shows a next pokemon of the list', () => {
+describe('5.', () => {
+  test('Pokedex have a button to reset filters', () => {
     const { getByText } = renderWithRouter(<App />);
     const nextButton = getByText(/Próximo pokémon/i);
+    const allButton = getByText(/All/i);
     allPokemons.forEach((pokemon) => {
       expect(getByText(pokemon.name)).toBeInTheDocument();
       fireEvent.click(nextButton);
     });
-    expect(getByText(allPokemons[0].name)).toBeInTheDocument();
+    fireEvent.click(allButton);
+    allPokemons.forEach((pokemon) => {
+      expect(getByText(pokemon.name)).toBeInTheDocument();
+      fireEvent.click(nextButton);
+    });
   });
 });
