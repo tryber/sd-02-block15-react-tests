4e15eb46f086ca93858e8993160d84f8391110a1
richardwellerson <richardwellerson@gmail.com>
2020-03-02 09:48:04 -0300

    Retirando teste do App.test.js e inserindo na pasta correta

---
+++ src/tests/1.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { cleanup } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
+
+afterEach(cleanup);
+
+test('renders a reading with the text `Pokédex`', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const heading = getByText(/Pokédex/i);
+  expect(heading).toBeInTheDocument();
+});
