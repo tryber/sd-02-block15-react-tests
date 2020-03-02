0788185c90ae76bb539660590cc07e6ca474b788
richardwellerson <richardwellerson@gmail.com>
2020-03-02 15:56:26 -0300

    Correçao do teste 8

--- src/tests/8.test.js
+++ src/tests/8.test.js
@@ -8,15 +8,18 @@ afterEach(cleanup);
 
 describe('8.', () => {
   test('Pokedex shows name, type and average weight of each pokemon', () => {
-    const { getByText, queryAllByText, queryByText } = renderWithRouter(<App />);
+    const { getByText, queryAllByText, queryByText, getByAltText } = renderWithRouter(<App />);
     const nextButton = getByText(/Próximo pokémon/i);
     allPokemons.forEach((pokemon) => {
       const pokemonName = getByText(pokemon.name);
       const pokemonType = queryAllByText(pokemon.type)[0];
       const pokemonWeight = queryByText(`Average weight: ${pokemon.averageWeight.value} ${pokemon.averageWeight.measurementUnit}`);
+      const pokemonAltImage = getByAltText(`${pokemon.name} sprite`);
       expect(pokemonName).toBeInTheDocument();
       expect(pokemonType).toBeInTheDocument();
       expect(pokemonWeight).toBeInTheDocument();
+      expect(pokemonAltImage).toBeInTheDocument();
+      expect(pokemonAltImage.src === pokemon.image).toBeTruthy();
       fireEvent.click(nextButton);
     });
   });
