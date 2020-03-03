9b2fbea28f0597770bd7a69504c564142cef93d1
Douglas Henrique <doughrqe@gmail.com>
2020-03-03 17:50:35 -0300

    requisito 27 sem teste

---
+++ src/App5.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { createMemoryHistory } from 'history';
+import { Router, MemoryRouter } from 'react-router-dom';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import App from './App';
+
+
+afterEach(cleanup);
+
+describe('Bonus', () => {
+  test('Locations Link', () => {
+    const history = createMemoryHistory();
+    const { getByText } = render(
+      <Router history={history}>
+        <App />
+      </Router>,
+    );
+    expect(getByText('Locations')).toBeInTheDocument();
+    expect(getByText('Locations').href).toBe('http://localhost/locations');
+    fireEvent.click(getByText('Locations'));
+    expect(history.location.pathname).toBe('/locations');
+  });
+})
---
+++ src/components/Locations.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+class Locations extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: [],
+      offset: 20,
+      controle: 0,
+    };
+    this.nextButton = this.nextButton.bind(this);
+    this.previousButton = this.previousButton.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('https://pokeapi.co/api/v2/ability/?offset=20&limit=20')
+      .then((resolve) => resolve.json())
+      .then(({ results, count }) => this.setState({
+        results: results.filter(({ name }) => name),
+        controle: count,
+      }));
+  }
+
+  nextButton() {
+    const { offset } = this.state;
+    fetch(`https://pokeapi.co/api/v2/ability/?offset=${offset + 20}&limit=20`)
+      .then((resolve) => resolve.json())
+      .then(({ results }) => this.setState((state) => ({
+        results: results.filter(({ name }) => name), offset: state.offset + 20,
+      })));
+  }
+
+  previousButton() {
+    const { offset } = this.state;
+    fetch(`https://pokeapi.co/api/v2/ability/?offset=${offset - 20}&limit=20`)
+      .then((resolve) => resolve.json())
+      .then(({ results }) => this.setState((state) => ({
+        results: results.filter(({ name }) => name), offset: state.offset - 20,
+      })));
+  }
+
+  render() {
+    const { results, offset,controle } = this.state;
+    return (
+      <div>
+        <button disabled={(offset === 20) ? !false : false} onClick={this.previousButton} type="button">Previous Page</button>
+        <button disabled={(offset >= controle - 20) ? !false : false} onClick={this.nextButton} type="button">Next Page</button>
+        {results.map(({ name }) => (
+          <div key={name}>
+            <h2>{name}</h2>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+
+export default Locations;
