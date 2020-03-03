860c875a3abd8970a515ca0bbdaacb32ecdf5a89
Giorgia <giorgialeoniac@gmail.com>
2020-03-03 18:49:58 -0300

    Testes relacionados a Pokedex enviados para o arquivo de testes da Pokedex para não ficar tão bagunçado no App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render, cleanup, fireEvent } from '@testing-library/react';
-import pokemons from './services/pokemons';
+import { render, cleanup } from '@testing-library/react';
 import App from './App';
 
 afterEach(cleanup);
@@ -24,55 +23,4 @@ test('1 - Shows the Pokedéx when the route is `/`', () => {
   );
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
-});
-
-test('2 - Pokedéx must show ONLY ONE Pokémon at a time', () => {
-  const { queryAllByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const weight = queryAllByText(/Average weight:/i);
-
-  expect(weight.length).toBe(1);
-});
-
-describe('3 - Testing "Próximo pokémon" button', () => {
-  it('3 - When pressing the next button, the page should display the next pokémon in the list', () => {
-    const { getByText } = render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-    );
-    const nextPokemonBtn = getByText(/Próximo pokémon/);
-
-    pokemons.forEach(({ name }) => {
-      expect(getByText(name)).toBeInTheDocument();
-      fireEvent.click(nextPokemonBtn);
-    });
-  });
-
-  it('The button should contain the text "Próximo Pokémon', () => {
-    const { getByText } = render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-    );
-    const nextPokemonBtn = getByText(/Próximo pokémon/);
-
-    expect(nextPokemonBtn).toBeInTheDocument();
-    expect(nextPokemonBtn.tagName).toBe('BUTTON');
-  });
-
-  it('Upon reaching the last Pokémon on the list, the Pokédex must return to the first Pokémon at the press of the button', () => {
-    const { getByText } = render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-    );
-    const nextPokemonBtn = getByText(/Próximo pokémon/);
-
-    pokemons.forEach(() => fireEvent.click(nextPokemonBtn));
-    expect(getByText(pokemons[0].name)).toBeInTheDocument();
-  });
 });
--- src/components/Pokedex.test.js
+++ src/components/Pokedex.test.js
@@ -1,1 +1,67 @@
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import pokemons from '../services/pokemons';
+import App from '../App';
+
+afterEach(cleanup);
+
+test('2 - Pokedéx must show ONLY ONE Pokémon at a time', () => {
+  const { queryAllByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const weight = queryAllByText(/Average weight:/i);
+
+  expect(weight.length).toBe(1);
+});
+
+describe('3 - Testing "Próximo pokémon" button', () => {
+  it('3 - When pressing the next button, the page should display the next pokémon in the list', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const nextPokemonBtn = getByText(/Próximo pokémon/);
+
+    pokemons.forEach(({ name }) => {
+      expect(getByText(name)).toBeInTheDocument();
+      fireEvent.click(nextPokemonBtn);
+    });
+  });
+
+  it('The button should contain the text "Próximo Pokémon', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const nextPokemonBtn = getByText(/Próximo pokémon/);
+
+    expect(nextPokemonBtn).toBeInTheDocument();
+    expect(nextPokemonBtn.tagName).toBe('BUTTON');
+  });
+
+  it('Upon reaching the last Pokémon on the list, the Pokédex must return to the first Pokémon at the press of the button', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const nextPokemonBtn = getByText(/Próximo pokémon/);
+
+    pokemons.forEach(() => fireEvent.click(nextPokemonBtn));
+    expect(getByText(pokemons[0].name)).toBeInTheDocument();
+  });
+});
+
+// describe('4 - The Pokédex must contain filter buttons', () => {
+//   it('The button text must be the type name, p. ex. "Psychic"', () => expect().toBe(String));
+//   const { getAllByText, getByText } = render(
+//     <MemoryRouter>
+//       <App />
+//     </MemoryRouter>,
+//   );
+//});
