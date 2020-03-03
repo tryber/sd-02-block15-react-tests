85b8217df1809a430e90ba37d884620f6f496805
Giorgia <giorgialeoniac@gmail.com>
2020-03-03 17:34:30 -0300

    Requisitos feitos até o 3, criado um arquivo 'Pokemons' onde ficarão os dados a serem consultados como a API e criado o arquivo de testes para Pokedex

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import App from './App';
+
+afterEach(cleanup);
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -13,7 +15,7 @@ test('renders a reading with the text `Pokédex`', () => {
   expect(heading).toBeInTheDocument();
 });
 
-test('shows the Pokedéx when the route is `/`', () => {
+test('1 - Shows the Pokedéx when the route is `/`', () => {
   const { getByText } = render(
     <MemoryRouter initialEntries={['/']}>
       <App />
@@ -21,4 +23,38 @@ test('shows the Pokedéx when the route is `/`', () => {
   );
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+test('2 - Pokedéx must show ONLY ONE Pokémon at a time', () => {
+  const { queryAllByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const weight = queryAllByText(/Average weight:/i);
+  expect(weight.length).toBe(1);
+});
+
+describe('3 - Testing "Próximo pokémon" button', () => {
+  it('3 - When pressing the next button, the page should display the next pokémon in the list', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const nextPokemonBtn = getByText(/Próximo pokémon/);
+
+  });
+
+  it('The button should contain the text "Próximo Pokémon', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const nextPokemonBtn = getByText(/Próximo pokémon/);
+
+    expect(nextPokemonBtn).toBeInTheDocument();
+    expect(nextPokemonBtn.tagName).toBe('BUTTON');
+  });
 });
