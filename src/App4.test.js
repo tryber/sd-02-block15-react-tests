cfba034fd0720d44ca0ef03cce286e60a4e4aa2a
Douglas Henrique <doughrqe@gmail.com>
2020-03-03 01:11:19 -0300

    requisito 22

--- src/App4.test.js
+++ src/App4.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { createMemoryHistory } from 'history';
-import { Router } from 'react-router-dom';
+import { Router, MemoryRouter } from 'react-router-dom';
 import { render, cleanup, fireEvent } from '@testing-library/react';
+import FavoritePokemons from './components/FavoritePokemons';
 import App from './App';
 import pokemons from './data2';
+import { object } from 'prop-types';
 
 afterEach(cleanup);
 
@@ -19,6 +21,7 @@ describe('test', () => {
     expect(getByText('About').href).toBe('http://localhost/about');
     expect(getByText(/Favorite Pokémons/).href).toBe('http://localhost/favorites');
   });
+
   test('18, 19, 20', () => {
     const history = createMemoryHistory();
     const { getByText } = render(
@@ -33,10 +36,11 @@ describe('test', () => {
     fireEvent.click(getByText(/Favorite Pokémons/));
     expect(history.location.pathname).toBe('/favorites');
   });
+
   test('21', () => {
     const history = createMemoryHistory();
     const {
-      getByText, queryByText, getAllByTestId, getByAltText
+      getByText, queryByText, getAllByTestId, getByAltText,
     } = render(
       <Router history={history}>
         <App />
@@ -48,6 +52,26 @@ describe('test', () => {
     expect(getAllByTestId('paragrafo').length).toBe(2);
     expect(getByAltText('Pokédex')).toBeInTheDocument();
     expect(getByAltText('Pokédex').src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
 
+  test('22', () => {
+    const { queryByText } = render(
+      <MemoryRouter initialEntries={['/']}>
+        <FavoritePokemons pokemons={[]} />
+      </MemoryRouter>,
+    );
+    expect(queryByText('No favorite pokemon found')).toBeInTheDocument();
+  });
+
+  test('22.1', () => {
+    const favorite = pokemons.filter((pokemon) => (pokemon.id % 2 === 0));
+    const { queryByText } = render(
+      <MemoryRouter initialEntries={['/']}>
+        <FavoritePokemons pokemons={favorite} />
+      </MemoryRouter>,
+    );
+    favorite.forEach((pokemon) => {
+      expect(queryByText(pokemon.name)).toBeInTheDocument();
+    });
   });
 });
