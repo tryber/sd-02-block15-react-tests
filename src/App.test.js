761422ca86194a376bc9ba5ec0b5f5fbed925d74
lauro <laurolyra@gmail.com>
2020-03-03 13:28:46 -0300

    requisito 2 cumprido

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from './App';
+import { Pokemon } from './components/Pokemon';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -13,7 +15,7 @@ test('renders a reading with the text `Pokédex`', () => {
   expect(heading).toBeInTheDocument();
 });
 
-test('shows the Pokedéx when the route is `/`', () => {
+test('1- shows the Pokedéx when the route is `/`', () => {
   const { getByText } = render(
     <MemoryRouter initialEntries={['/']}>
       <App />
@@ -21,4 +23,14 @@ test('shows the Pokedéx when the route is `/`', () => {
   );
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+test('2- shows only one Pokemon on render', () => {
+  const { getAllByText } = render(
+    <MemoryRouter initialEntries={['/']}>
+      <App />
+    </MemoryRouter>,
+  );
+  const onePokemon = getAllByText('More details').length;
+  expect(onePokemon).toBe(1);
 });
