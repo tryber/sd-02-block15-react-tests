64314f4dbdac25f9491ab44c51b12bcb81a195b9
Douglas Henrique <doughrqe@gmail.com>
2020-03-02 23:01:18 -0300

    requisito 18, 19, 20

--- src/App4.test.js
+++ src/App4.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createMemoryHistory } from 'history';
-import { MemoryRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { render, cleanup, fireEvent } from '@testing-library/react';
 import App from './App';
 import pokemons from './data2';
@@ -9,13 +9,28 @@ afterEach(cleanup);
 
 describe('test', () => {
   test('At the top of the application, there should be a fixed set of navigation links', () => {
+    const history = createMemoryHistory();
     const { getByText } = render(
-      <MemoryRouter>
+      <Router history={history}>
         <App />
-      </MemoryRouter>,
+      </Router>,
     );
     expect(getByText('Home').href).toBe('http://localhost/');
     expect(getByText('About').href).toBe('http://localhost/about');
     expect(getByText(/Favorite Pokémons/).href).toBe('http://localhost/favorites');
+  });
+  test('18, 19, 20', () => {
+    const history = createMemoryHistory();
+    const { getByText } = render(
+      <Router history={history}>
+        <App />
+      </Router>,
+    );
+    fireEvent.click(getByText('Home'));
+    expect(history.location.pathname).toBe('/');
+    fireEvent.click(getByText('About'));
+    expect(history.location.pathname).toBe('/about');
+    fireEvent.click(getByText(/Favorite Pokémons/));
+    expect(history.location.pathname).toBe('/favorites');
   });
 });
