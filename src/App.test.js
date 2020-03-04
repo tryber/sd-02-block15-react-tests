07c810bb5ce74988cabc4427c1e7a23c37f63b86
ayandram <ayandram7727@gmail.com>
2020-03-04 04:02:53 -0300

    Feito ate o requisito 3

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { MemoryRouter, Router } from 'react-router-dom';
-import { render, fireEvent, cleanup } from '@testing-library/react';
+import { render, fireEvent, cleanup, getAllByTestId } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
 import App from './App';
+
+const arrayPokemons = [
+  { name: 'Pikachu', type: 'Electric', averageWeight: '6.0 kg' },
+  { name: 'Charmander', type: 'Fire', averageWeight: '8.5 kg' },
+  { name: 'Caterpie', type: 'Bug', averageWeight: '2.9 kg' },
+  { name: 'Ekans', type: 'Poison', averageWeight: '6.9 kg' },
+  { name: 'Alakazam', type: 'Psychic', averageWeight: '48.0 kg' },
+  { name: 'Mew', type: 'Psychic', averageWeight: '4.0 kg' },
+  { name: 'Rapidash', type: 'Fire', averageWeight: '95.0 kg' },
+  { name: 'Snorlax', type: 'Normal', averageWeight: '460.0 kg' },
+  { name: 'Dragonair', type: 'Dragon', averageWeight: '16.5 kg' },
+];
 
 function renderWithRouter(
   ui,
@@ -85,5 +97,43 @@ describe('2 - A Pokédex deve exibir apenas um pokémon por vez', () => {
       const arrayPokemonImage = getAllByTestId('pokemon-image');
       expect(arrayPokemonImage.length).toBe(1);
     }
+  });
+});
+
+describe('3 - Ao apertar o botão de próximo, a página deve exibir o próximo pokémon da lista', () => {
+  test('O botão deve conter o texto "Próximo pokémon"', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    const botaoProximoPokemon = getByText('Próximo pokémon');
+    expect(botaoProximoPokemon).toBeInTheDocument();
+  });
+
+  test('Cliques sucessivos no botão devem mostrar o próximo pokémon da lista', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    const botaoProximoPokemon = getByText('Próximo pokémon');
+
+    for (let i = 0; i < 8; i += 1) {
+      fireEvent.click(botaoProximoPokemon);
+
+      expect(botaoProximoPokemon).toBeInTheDocument();
+
+      const pokemonName = getByText(arrayPokemons[i + 1].name);
+      expect(pokemonName).toBeInTheDocument();
+    }
+  });
+
+  test('Ao se chegar ao último pokémon da lista, a Pokédex deve voltar para o primeiro pokémon no apertar do botão', () => {
+    const { getByText, queryByText } = renderWithRouter(<App />);
+
+    const botaoProximoPokemon = getByText('Próximo pokémon');
+
+    while (queryByText(arrayPokemons[arrayPokemons.length - 1].name) === null) {
+      fireEvent.click(botaoProximoPokemon);
+    }
+
+    fireEvent.click(botaoProximoPokemon);
+
+    expect(getByText(arrayPokemons[0].name)).toBeInTheDocument();
   });
 });
