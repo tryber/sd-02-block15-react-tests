5ff552adbf21d7e658c6914b6f9206264a2a565d
richardwellerson <richardwellerson@gmail.com>
2020-03-04 18:08:19 -0300

    Teste 17 feito

--- src/tests/17.test.js
+++ src/tests/17.test.js
@@ -1,3 +1,19 @@
 import React from 'react';
-import allPokemons from '../services/pokedexDataTest';
 import { cleanup } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
+
+afterEach(cleanup);
+
+describe('17.', () => {
+  test('Pokedex header needs three links', () => {
+    const { queryByText } = renderWithRouter(<App />);
+    const homeText = queryByText(/Home/i);
+    const aboutText = queryByText(/About/i);
+    const favoritePokemonsText = queryByText(/Favorite Pokémons/i);
+    const localHost = 'http://localhost';
+    expect(homeText.href).toBe(`${localHost}/`);
+    expect(aboutText.href).toBe(`${localHost}/about`);
+    expect(favoritePokemonsText.href).toBe(`${localHost}/favorites`);
+  });
+});
