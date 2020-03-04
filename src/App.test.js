6b93d36675b2f6b5dc674c433a6c62cfb00ca384
EddyeBoy27 <lipe0503@gmail.com>
2020-03-04 16:59:41 -0300

    Finalizado req 1, 2 e 3. Iniciado 4

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, fireEvent, getByText } from '@testing-library/react';
+import pokemons from './types/mockPokemons';
 import App from './App';
 
 afterEach(cleanup)
 
-test('renders a reading with the text `Pokédex`', () => {
+test('1 - renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
     <MemoryRouter>
       <App />
@@ -15,12 +16,46 @@ test('renders a reading with the text `Pokédex`', () => {
   expect(heading).toBeInTheDocument();
 });
 
-test('shows the Pokedéx when the route is `/`', () => {
+test('2 - shows the Pokedéx when the route is `/`', () => {
   const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
+    <MemoryRouter initialEntries={[{ pathname: "/" }]}>
       <App />
     </MemoryRouter>,
   );
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
 });
+
+test('3 - show next Pokemon when click on button - Próximo pokémon -', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  
+  const buttonNxtPkm = getByText(/Próximo pokémon/i, {selector: 'button'});
+  expect(buttonNxtPkm).toBeInTheDocument();
+  pokemons.forEach((pokemon) => {
+    expect(getByText(pokemon.name)).toBeInTheDocument();
+    fireEvent.click(buttonNxtPkm);
+  });
+  expect(getByText(pokemons[0].name)).toBeInTheDocument();
+})
+
+test('4 - Pokedex need filters to map each pokemon by type', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+
+  const buttonNxtPkm = getByText(/Próximo pokémon/i, {selector: 'button'});
+  const allPkm = getByText(/All/i);
+  pokemons.forEach((pokemon) => {
+    const { type, name } = pokemon; 
+    expect(getByText(type, {selector: 'button'})).toBeInTheDocument();
+    if (buttonNxtPkm)
+    fireEvent.click(buttonNxtPkm);
+    expect()
+  })
+})
