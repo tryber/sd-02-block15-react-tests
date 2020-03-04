2f4e90ae7d43b22177454096e1260b9919913faa
ayandram <ayandram7727@gmail.com>
2020-03-04 01:52:58 -0300

    Feito ate requisito 2

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,89 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { MemoryRouter, Router } from 'react-router-dom';
+import { render, fireEvent, cleanup } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
 import App from './App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+function renderWithRouter(
+  ui,
+  { route = '/', history = createMemoryHistory({ initialEntries: [route] }) } = {},
+) {
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    history,
+  };
+}
+
+afterEach(cleanup);
+
+describe('0 - Teste inicial', () => {
+  test('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+});
+
+describe('1 - Ao carregar a aplicação no caminho de URL “/”, a página principal da Pokédex deve ser mostrada', () => {
+  test('shows the Pokedéx when the route is `/`', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>,
+    );
+  
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  });
+});
+
+describe('2 - A Pokédex deve exibir apenas um pokémon por vez', () => {
+  test('exibe apenas um pokémon na primeira renderização', () => {
+    const { getAllByText, getAllByTestId } = renderWithRouter(<App />);
+    const arrayAverageWeight = getAllByText(/Average weight:/);
+    expect(arrayAverageWeight.length).toBe(1);
+
+    const arrayMoreDetails = getAllByText('More details');
+    expect(arrayMoreDetails.length).toBe(1);
+
+    const arrayPokemonName = getAllByTestId('pokemon-name');
+    expect(arrayPokemonName.length).toBe(1);
+
+    const arrayPokemonType = getAllByTestId('pokemon-type');
+    expect(arrayPokemonType.length).toBe(1);
+
+    const arrayPokemonImage = getAllByTestId('pokemon-image');
+    expect(arrayPokemonImage.length).toBe(1);
+  });
+
+  test('exibe apenas um pokémon ao clicar em vários botões aleatoriamente dentro da mesma página', () => {
+    const { getAllByText, getAllByTestId } = renderWithRouter(<App />);
+
+    const botoes = document.getElementsByTagName('button');
+
+    for (let i = 0; i < 100; i += 1) {
+      const indexAleatorio = Math.round(Math.random() * 8);
+      const botao = botoes[indexAleatorio];
+  
+      fireEvent.click(botao);
+  
+      const arrayAverageWeight = getAllByText(/Average weight:/);
+      expect(arrayAverageWeight.length).toBe(1);
+  
+      const arrayMoreDetails = getAllByText('More details');
+      expect(arrayMoreDetails.length).toBe(1);
+  
+      const arrayPokemonName = getAllByTestId('pokemon-name');
+      expect(arrayPokemonName.length).toBe(1);
+    
+      const arrayPokemonType = getAllByTestId('pokemon-type');
+      expect(arrayPokemonType.length).toBe(1);
+    
+      const arrayPokemonImage = getAllByTestId('pokemon-image');
+      expect(arrayPokemonImage.length).toBe(1);
+    }
+  });
 });
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -17,12 +17,12 @@ class Pokemon extends React.Component {
     return (
       <div className="pokemon">
         <div className="pokemon-overview">
-          <p>{name}</p>
-          <p>{type}</p>
+          <p data-testid="pokemon-name">{name}</p>
+          <p data-testid="pokemon-type">{type}</p>
           <p>{`Average weight: ${value} ${measurementUnit}`}</p>
           {showDetailsLink && <Link to={`pokemons/${id}`}>More details</Link>}
         </div>
-        <img src={image} alt={`${name} sprite`} />
+        <img data-testid="pokemon-image" src={image} alt={`${name} sprite`} />
         {isFavorite && (
           <img
             className="favorite-icon"
