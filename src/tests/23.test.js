5a138eef8d71d87901434b638fae80ad4f4ca6d7
richardwellerson <richardwellerson@gmail.com>
2020-03-04 23:45:40 -0300

    Corrigindo teste 23, faltou um requisito

--- src/tests/23.test.js
+++ src/tests/23.test.js
@@ -8,9 +8,13 @@ afterEach(cleanup);
 describe('23.', () => {
   test('When the route not exist, pokedex shows H2 with text', () => {
     const route = '/randomic';
-    const { getByText } = renderWithRouter(<App />, { route });
+    const { getByText, getByAltText } = renderWithRouter(<App />, { route });
     const notFoundTag = getByText(/Page requested not found/i);
     expect(notFoundTag).toBeInTheDocument();
     expect(notFoundTag.tagName).toBe('H2');
+
+    const imageNotFound = getByAltText('Pikachu crying because the page requested was not found');
+    expect(imageNotFound).toBeInTheDocument();
+    expect(imageNotFound.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
   });
 });
