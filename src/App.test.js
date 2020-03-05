92c158a67861934dfe59bb19b817d33facef50c0
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-03-05 13:25:32 -0300

    Requisito 11

--- src/App.test.js
+++ src/App.test.js
@@ -17,6 +17,7 @@ describe('Pokemon app.js tests', () => {
     const heading = getByText(/Pokédex/i);
     expect(heading).toBeInTheDocument();
   });
+
   test('10 - Ao clicar no link de navegação do pokémon, a aplicação deve ser redirecionada...', () => {
     const history = createMemoryHistory();
     const { getByText, queryByText } = render(
@@ -29,6 +30,40 @@ describe('Pokemon app.js tests', () => {
       const page = `/pokemons/${pokemon.id}`;
       fireEvent.click(moreDetails);
       expect(history.location.pathname).toBe(page);
+      history.push('/');
+      for (let i = 0; i <= index; i += 1) {
+        const nextBtn = getByText(/Próximo pokémon/i);
+        fireEvent.click(nextBtn);
+      }
+    });
+  });
+
+  test('11 - A página de detalhes de pokémon deve exibir o nome, tipo, peso médio e imagem...', () => {
+    const history = createMemoryHistory();
+    const {
+      getByText, getByAltText, queryByText, queryByAltText,
+    } = render(
+      <Router history={history}>
+        <App />
+      </Router>,
+    );
+    pokemonsMock.forEach((pokemon, index) => {
+      const {
+        id, name, type, averageWeight: { value, measurementUnit }, image,
+      } = pokemon;
+      const imageElem = getByAltText(`${name} sprite`);
+      const page = `/pokemons/${id}`;
+      fireEvent.click(queryByText('More details'));
+      const weightText = queryByText(`Average weight: ${value} ${measurementUnit}`);
+      expect(history.location.pathname).toBe(page);
+      // Exibindo nome e tipo do pokémon
+      expect(queryByText(name)).toBeInTheDocument();
+      expect(queryByText(type)).toBeInTheDocument();
+      // O peso médio do pokémon deve ser exibido com um texto no formato...
+      expect(weightText).toBeInTheDocument();
+      // A imagem deve conter um atributo src com a URL da imagem do pokémon...
+      expect(queryByAltText(`${name} sprite`)).toBeInTheDocument();
+      expect(imageElem.src).toBe(image);
       history.push('/');
       for (let i = 0; i <= index; i += 1) {
         const nextBtn = getByText(/Próximo pokémon/i);
