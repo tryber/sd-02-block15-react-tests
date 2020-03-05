b76bea49cc88580dc0f59c79a862e18a23ca278e
guicgs <etohlif@protonmail.com>
2020-03-05 13:05:36 -0300

    Requisito 21 concluído

--- src/components/PokemonDetails.test.js
+++ src/components/PokemonDetails.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { MemoryRouter, Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
-import { render, fireEvent, cleanup, getAllByAltText } from '@testing-library/react';
+import { render, fireEvent, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import App from '../App';
 import pokemons from '../data';
