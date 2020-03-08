b5a39463e38bf7b2b9762a52c7c8cc831b25751f
guicgs <etohlif@protonmail.com>
2020-03-08 12:19:32 -0300

    refatorando teste 22

--- src/components/FavoritePokemons.test.js
+++ src/components/FavoritePokemons.test.js
@@ -3,12 +3,13 @@ import { MemoryRouter } from 'react-router-dom';
 import { render, fireEvent, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import App from '../App';
+import pokemons from '../data';
 
 afterEach(cleanup);
 
 describe('The favorite Pokémon page should display favorite Pokémon', () => {
   it('The page should display all favorite Pokémon and should not display any non-favored Pokémon.', () => {
-    const { getByText, queryByText, getByLabelText } = render(
+    const { getByText, queryByText, getByLabelText, getAllByTestId } = render(
       <MemoryRouter>
         <App />
       </MemoryRouter>,
@@ -20,13 +21,14 @@ describe('The favorite Pokémon page should display favorite Pokémon', () => {
     fireEvent.click(getByText(/More details/i));
     fireEvent.click(getByLabelText(/Pokémon favoritado?/));
     fireEvent.click(getByText(/Favorite Pokémons/i));
-    expect(getByText('Pikachu')).toBeInTheDocument();
-    expect(getByText('Charmander')).toBeInTheDocument();
-    expect(queryByText('Caterpie')).not.toBeInTheDocument();
-    expect(queryByText('Alakazam')).not.toBeInTheDocument();
-    expect(queryByText('Mew')).not.toBeInTheDocument();
-    expect(queryByText('Rapidash')).not.toBeInTheDocument();
-    expect(queryByText('Snorlax')).not.toBeInTheDocument();
-    expect(queryByText('Dragonair')).not.toBeInTheDocument();
+    pokemons.forEach(({ name }) => {
+      if (queryByText(name)) {
+        expect(queryByText(name)).toBeInTheDocument();
+        expect(getAllByTestId('pokemon').length).toEqual(2);
+        expect(getAllByTestId('star-icon').length).toEqual(2);
+      } else {
+        expect(queryByText(name)).not.toBeInTheDocument();
+      }
+    });
   });
 });
