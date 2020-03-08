c1063666492539a8fe1c044dc932feed6ebca29f
EddyeBoy27 <lipe0503@gmail.com>
2020-03-08 10:47:11 -0300

    Finalizado ate requisito 6

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import { MemoryRouter } from 'react-router-dom';
 import { render, cleanup, fireEvent, getByText } from '@testing-library/react';
 import pokemons from './types/mockPokemons';
 import App from './App';
+import '@testing-library/jest-dom'
 
 afterEach(cleanup)
 
@@ -50,10 +51,56 @@ test('4 - Pokedex need filters to map each pokemon by type', () => {
   );
 
   const buttonNxtPkm = getByText(/Próximo pokémon/i, {selector: 'button'});
-  const allPkm = getByText(/All/i);
-  pokemons.forEach((pokemon) => {
-    const { type, name } = pokemon; 
+  const typesfiltered = [...new Set(pokemons.map((pokemon) =>  pokemon.type))];
+  typesfiltered.forEach((type) => {
     expect(getByText(type, {selector: 'button'})).toBeInTheDocument();
-    console.log(pokemon.type)
-  })
+    fireEvent.click(getByText(type, {selector: 'button'}));
+    const pkmnFiltered = pokemons.filter((pokemon) => pokemon.type === type);
+    if (pkmnFiltered.length > 1) {
+      for (let i = 0; pkmnFiltered.length > i; i += 1) {
+        const pkmnMapped = pokemons.filter((pokemon) => pokemon.type === type);
+        expect(getByText(pkmnMapped[i].name, {selector: 'p'})).toBeInTheDocument();
+        fireEvent.click(buttonNxtPkm);
+      }
+    }
+  });
+})
+
+test('5 - Pokedex needs a button All to reset filter', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+
+  const buttonAll = getByText(/All/i, {selector: 'button'});
+  const buttonNxtPkm = getByText(/Próximo pokémon/i, {selector: 'button'});
+  expect(buttonAll).toBeInTheDocument();
+  fireEvent.click(buttonAll);
+  pokemons.forEach((pokemon) => {
+    expect(getByText(pokemon.name)).toBeInTheDocument();
+    fireEvent.click(buttonNxtPkm);
+  });
+})
+
+test('6 - Pokedex needs to generate a filter button for each type of pokemon', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  
+  const buttonAll = getByText(/All/i, {selector: 'button'});
+  const typesfiltered = [...new Set(pokemons.map((pokemon) =>  pokemon.type))];
+  const buttonNxtPkm = getByText(/Próximo pokémon/i, {selector: 'button'});
+  typesfiltered.forEach((type) => {
+    expect(getByText(type, {selector: 'button'})).toBeInTheDocument();
+    fireEvent.click(getByText(type, {selector: 'button'}));
+    expect(buttonAll).toBeInTheDocument();
+  });
+  fireEvent.click(buttonAll);
+  pokemons.forEach((pokemon) => {
+    expect(getByText(pokemon.name)).toBeInTheDocument();
+    fireEvent.click(buttonNxtPkm);
+  });
 })
