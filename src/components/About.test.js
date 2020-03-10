65094bac291c9b4585283ac9a10fbfce5f61202e
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-03-10 12:30:43 -0300

    Refatoração

--- src/components/About.test.js
+++ src/components/About.test.js
@@ -18,10 +18,10 @@ describe('Pokemon About.js tests', () => {
     const page = '/about';
     expect(history.location.pathname).toBe(page);
     // A página deve conter um heading h2 com o texto About Pokédex
-    expect(queryByText('About Pokédex', { selector: 'h2' }));
+    expect(queryByText(/About Pokédex/i, { selector: 'h2' })).toBeInTheDocument();
     // A página deve conter dois parágrafos com texto sobre a Pokédex
-    expect(queryByText('This application simulates a Pokédex, a digital encliclopedia containing all Pokémons ', { selector: 'p' }));
-    expect(queryByText('One can filter Pokémons by type, and see more details for each one of them', { selector: 'p' }));
+    expect(queryByText(/This application simulates a Pokédex, a digital encliclopedia containing all Pokémons/i, { selector: 'p' })).toBeInTheDocument();
+    expect(queryByText(/One can filter Pokémons by type, and see more details for each one of them/i, { selector: 'p' })).toBeInTheDocument();
     // A página deve conter a seguinte imagem de uma Pokédex
     const imageDex = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
     expect(queryByAltText('Pokédex')).toBeInTheDocument();
