c7cd825e03684b7f1450fdd818c03653734454e3
Lucas-Werner <lucas.1werner@gmail.com>
2020-03-23 17:49:02 -0300

    test 2

--- src/App.test.js
+++ src/App.test.js
@@ -2,30 +2,48 @@ import React from 'react';
 import { MemoryRouter, Router } from 'react-router-dom';
 import { render, cleanup, fireEvent } from '@testing-library/react';
 import App from './App';
-import Pokedex from './components/Pokedex';
-import FavoritePokemons from './components/FavoritePokemons';
-import mockData from './mockData';
+import { Pokedex, FavoritePokemons } from './components';
+import pokemons from './mockData';
 
-test('renders a Heading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  )
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+afterEach(cleanup);
+
+const pokemonsName = pokemons.map(({ name }) => name);
+const pokemonsType = pokemons.map(({ type }) => type);
+
+describe('Test 1 - shows pokedex in main page', () => {
+  test('1.1 renders a heading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('1.2 shows the Pokedéx when the route is `/`', () => {
+    const { getByText } = render(
+      <MemoryRouter
+        initialEntries={['/']}
+      >
+        <App />
+      </MemoryRouter>,
+    );
+
+    expect(
+      getByText('Encountered pokémons'),
+    ).toBeInTheDocument();
+  });
 });
 
-test('shows the Pokedéx when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter
-      initialEntries={['/']}
-    >
-      <App />
-    </MemoryRouter>,
-  );
-
-  expect(
-    getByText('Encountered pokémons'),
-  ).toBeInTheDocument();
+describe('Test 2 - only one pokemon each page', () => {
+  test('shows only one pokemon at once', () => {
+    const { getAllByText } = render(
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>,
+    );
+    expect(getAllByText(/more details/i).length.toBe(1));
+    expect(getAllByText(/more details/i)[1].toBeUndefined());
+  });
 });
