235e7dd95597c1cb0d71109c284de49a015a213f
Lucas-Werner <lucas.1werner@gmail.com>
2020-03-25 23:39:03 -0300

    double files corrected and proptypes

--- src/RenderWithRouter.js
+++ src/RenderWithRouter.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
@@ -8,11 +7,8 @@ function renderWithRouter(
   ui,
   { route = '/', history = createMemoryHistory({ initialEntries: [route] }) } = {},
 ) {
-  const Wrapper = ({ children }) => (
-    <Router history={history}>{children}</Router>
-  );
   return {
-    ...render(ui, { wrapper: Wrapper }),
+    ...render(<Router history={history}>{ui}</Router>),
     // adding `history` to the returned utilities to allow us
     // to reference it in our tests (just try to avoid using
     // this to test implementation details).
